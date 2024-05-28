58f480dbdefa57a85e8e23821aa31af99d29b417
Ethan <108598670+echo-lalia@users.noreply.github.com>
2024-05-27 18:56:11 -0700

    Update example_interrupt.py

--- example_interrupt.py
+++ example_interrupt.py
@@ -1,7 +1,6 @@
 import micropython
 
 import gt911
-import gt911_constants as gt
 
 micropython.alloc_emergency_exception_buf(100)
 
@@ -15,7 +14,7 @@ def on_touch(_):
 
 
 tp = gt911.GT911(sda="PB7", scl="PB6", interrupt="PB4", reset="PB3")
-tp.begin(gt.Addr.ADDR1)
+tp.begin(gt911.Addr.ADDR1)
 print("Finished initialization.")
 print(f"  Screen: {tp.width}x{tp.height}")
 
